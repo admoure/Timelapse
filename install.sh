7992572df4aac3559aae19a6375413ddf3a5e65e
admoure <admoure.mail@gmail.com>
2014-07-28 16:58:02 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 mkdir /home/pi/timelapse/
 cp ./timelapse/*.* /home/pi/timelapse/
 cd /home/pi/timelapse/
-chmod 777 /home/pi/soft_inclinometro/*.*
+chmod 777 /home/pi/timelapse/*.*
 gcc -o timelapse timelapse.c -l bcm2835
 cd -
 cp ./www/*.* /var/www/
