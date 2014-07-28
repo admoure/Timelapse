215a4183fe136d2144b15b466e10663304ffcc0d
David Moure <admoure.mail@gmail.com>
2014-07-28 16:54:31 +0100

    instalador

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+########################################
+#############instalador#################
+########################################
+
+mkdir /home/pi/timelapse/
+cp ./timelapse/*.* /home/pi/timelapse/
+cd /home/pi/timelapse/
+chmod 777 /home/pi/soft_inclinometro/*.*
+gcc -o timelapse timelapse.c -l bcm2835
+cd -
+cp ./www/*.* /var/www/
+chmod 777 /var/www/*.*
+crontab ./varios/cron.txt
